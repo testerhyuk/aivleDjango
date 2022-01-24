0e56d013bd39b3a6db9e6ef321cc67ca5b28087d
hyuk <testerhyuk214@gmail.com>
2022-01-24 13:14:02 +0900

    main

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', config_views.home, name='home'),
     path('home/', include('triceps.urls')),
-    path('member/', include('member.urls'), name='member'),
 ]
