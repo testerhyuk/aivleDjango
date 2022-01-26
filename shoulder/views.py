0a056f260deec12b250c50a318795fd148c17d51
ringolove <mybloodyvalentine@outlook.kr>
2022-01-26 17:46:11 +0900

    ..

--- shoulder/views.py
+++ shoulder/views.py
@@ -7,12 +7,8 @@ def shoulder(request):
 
 def update_count(request):
     if request.method == 'POST':
-        shoulder = request.POST.get('ud_count')
-        print(shoulder)
-        t = History(shoulder=shoulder)
-        t.save()
-
         member_id = request.session['member_id']
-        m = History(member_id=Member.objects)
-        m.save()
+        shoulder = request.POST.get('ud_count')
+        t = History(shoulder=shoulder, member_id=Member.objects.get(member_id = member_id))
+        t.save()
     return redirect('home')
