e5184160b7373164f5aa2bb4d66d4f2882647970
hyuk <testerhyuk214@gmail.com>
2022-01-26 16:08:26 +0900

    history db 연결

--- triceps/views.py
+++ triceps/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from member.models import Member
+from history.models import History
 
 def triceps(request):
     return render(request, 'triceps/triceps.html')
+
+def update_count(request):
+    if request.method == 'POST':
+        triceps = request.POST.get('ud_count')
+        t = History(triceps=triceps)
+        t.save()
+
+        member_id = request.session['member_id']
+        m = History(member_id=Member.objects)
+        m.save()
+    return redirect('home')
