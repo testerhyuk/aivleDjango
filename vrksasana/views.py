1cc6f12e0056a86d6066733d62f4ec8d68fbbd3f
ringolove <mybloodyvalentine@outlook.kr>
2022-01-26 17:21:19 +0900

    카운팅 오류 다 같이 검토

--- vrksasana/views.py
+++ vrksasana/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+
 
 # Create your views here.
 def vrksasana(request):
     return render(
         request, 'vrksasana/vrksasana.html')
+    
+# def update_count(request):
+#     if request.method == 'POST':
+#         member_id = request.session['member_id']
+#         vrksasana = request.POST.get('ud_count')
+#         t = History
+#         )
