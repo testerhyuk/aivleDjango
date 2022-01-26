00ffc99728d77459ee2fa4e091a794debed9231f
yongjunism <yjcho.dev@gmail.com>
2022-01-26 22:59:09 +0900

    db로 카운트  넘기기

--- squat/views.py
+++ squat/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from member.models import Member
+from history.models import History
 
 def squat(request):
     return render(request, './squat.html')
+
+def update_count(request):
+    if request.method == 'POST':
+        member_id = request.session['member_id']
+        triceps = request.POST.get('ud_count')
+        t = History(triceps=triceps, member_id=Member.objects.get(member_id = member_id))
+        t.save()
+
+    return redirect('home')
