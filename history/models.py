0a0c22d506e8eedd7f7829df1efad3fae9a05ca2
hyuk <testerhyuk214@gmail.com>
2022-01-26 12:12:22 +0900

    데이터베이스 다시설정

--- history/models.py
+++ history/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class History(models.Model):
+    name = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True)
+    triceps = models.IntegerField(null=True)
+    shoulder = models.IntegerField(null=True)
+    squat = models.IntegerField(null=True)
+    pullup = models.IntegerField(null=True)
+    vrksasana = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = 'history'
+        app_label = 'history'
+        managed = True
