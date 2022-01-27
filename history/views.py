d1a2ace0d97b88175d30b645e34a64c5dace9195
ringolove <mybloodyvalentine@outlook.kr>
2022-01-27 10:12:20 +0900

    기본 회원 정보

--- history/views.py
+++ history/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Member
 
 # Create your views here.
 def history(request):
+    record = Member.objects.all()
+        
     return render(
-        request, 'history/history.html')
+        request, 'history/history.html',
+        {'data': record}
+    )
