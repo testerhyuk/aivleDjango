832ded9a75732f48bf97d061a9861c3a33130df8
yongjunism <yjcho.dev@gmail.com>
2022-01-27 09:56:17 +0900

    스쿼트 모델 삭제

--- squat/models.py
+++ squat/models.py
@@ -1,11 +1,3 @@
 from django.db import models
-from django.db.models.fields import IntegerField, DateField
 
-class Squat(models.Model):
-    exercise_date = DateField()
-    count = IntegerField()
-    # member_id = models.OneToOneField(Member, on_delete=models.CASCADE, null=True
-
-    class Meta:
-        db_table = "squat"
-        managed = False
+# Create your models here.
