0a5cbd854a7773664807d4d78b691453ae4ebf0e
hyuk <testerhyuk214@gmail.com>
2022-01-27 15:32:45 +0900

    .

--- history/urls.py
+++ history/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name='history'
 urlpatterns = [
     path('history/', views.history),
+<<<<<<< HEAD
     path('change_image/', views.change_image),
+=======
+    path('member_del/', views.member_del, name='member_del'),
+>>>>>>> e40ea7e62e6d403b5f73db4cb4f95641809f618e
 ]
