015cf8780eb26f1b2be4a96cf3219032a373c1c7
yongjunism <yjcho.dev@gmail.com>
2022-01-27 15:13:33 +0900

    회원탈퇴 기능(아직 버튼 꾸미고 위치만져야함)

--- history/urls.py
+++ history/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name='history'
 urlpatterns = [
     path('history/', views.history),
+    path('member_del/', views.member_del, name='member_del'),
 ]
--- history/views.py
+++ history/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Member
+from django.contrib.auth.hashers import check_password
+from django.contrib import messages
+
 
 # Create your views here.
 def history(request):
@@ -9,3 +12,29 @@ def history(request):
         request, 'history/history.html',
         {'data': record}
     )
+
+def member_del(request):
+    if request.method == "POST":
+        pw_del = request.POST["pw_del"]
+        m = Member.objects.filter(psw_rg=pw_del)
+
+        try:
+            m = Member.objects.get(member_id=request.session['member_id'], psw_rg=pw_del)
+        except:
+            msg = '비밀번호가 틀렸습니다.'
+            return render(request, 'history/member_del.html', { 'msg': msg })
+        else:
+            m.delete()
+            request.session.flush()
+            return redirect('home')
+        # for i in m:
+        #     if m[i].member_id == request.session['member_id']:
+        #         m.delete()
+        #         request.session.flush()
+        #         return redirect('home')
+        # m = request.m
+        # if check_password(pw_del, m.psw_rg):
+        #     m.delete()
+        # return redirect('home')
+    else:
+        return render(request, 'history/member_del.html')
